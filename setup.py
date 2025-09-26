39f56ba27bb503e94e2f8057966377aa18207801
Ahmad Gul <ahmadkhan20033@gmail.com>
2025-09-26 05:13:01 -0700

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version = "0.1.0",
+    author="Ahmad khan",
+    author_email="ahmadkhan20033@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
